f5fe6087748c46d69fc9739a80fd74da5d4b8c60
EmersonMR25 <89536495+EmersonMR25@users.noreply.github.com>
2021-09-16 12:09:21 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,18 @@
 # game.py
-
+import random
 print("Rock, Paper, Scissors, Shoot!")
 
-#PROMT USER FRO IMPUT
+#PROMT USER FOR IMPUT
 
 user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
+print("User chose:")
 print(user_choice)
+
+
+#COMPUTER CHOICE AT RANDOM TABLE
+
+computer_options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(computer_options)
+print("Computer chose:")
+print(computer_choice)
+
