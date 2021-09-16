a7b4e9d6ae20e6d7ae083decfdcf6c898cc7bf00
EmersonMR25 <89536495+EmersonMR25@users.noreply.github.com>
2021-09-16 15:03:34 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 # game.py
 import random
-print("Rock, Paper, Scissors, Shoot!")
+
+print("ROCK, PAPER, SCISSORS ... SHOOT!")
 
 #PROMT USER FOR IMPUT
 
-user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
-print("User chose:")
+user_choice = input("CHOOSE 'rock', 'paper' or 'scissors': ")
+print("USER HAS CHOSEN:")
 print(user_choice)
 
 
@@ -13,11 +14,30 @@ print(user_choice)
 
 computer_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(computer_options)
-print("Computer chose:")
+print("COMPUTER HAS CHOSEN:")
 print(computer_choice)
 
-# Determining THE WINNER
+# DETERMING THE WINNER
 
 if computer_choice == user_choice:
-    print("It is a tie")
-    
+    print("Both chose the same. It is a tie!")
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print("Rock smashes scissors. YOU WIN!")
+    else:
+        print("Paper covers rock. YOU LOOSE!")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print("Paper convers rock. YOU WIN!")
+    else:
+        print("Scissors cuts paper. YOU LOOSE!")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("Paper cuts paper. YOU WIN!")
+    else:
+        print("Rock smashes paper. YOU LOOSE!")
+
+
+# PLAY AGAIN 
+## (Got ir from GOOGLE)
+
